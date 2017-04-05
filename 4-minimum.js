f0f1b0683458161733c32a2d666b1c9d4960e4a5
Jules <jgmz.jg@gmail.com>
2017-04-05 17:22:56 -0500

    add solution

---
+++ 4-minimum.js
@@ -0,0 +1,11 @@
+/*
+Write a function min that takes two arguments and returns their minimum. Without using Math.min 
+*/
+
+function min(first,second){
+  if (first < second){
+    return first;
+  } else {
+    return second;
+  }
+}
